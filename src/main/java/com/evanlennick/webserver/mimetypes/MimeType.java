a07457ffb3bbaada43fea316f35441c36524559c
Evan Lennick <evan.lennick@accolade.com>
2016-01-06 22:24:13 -0500

    more mimetypes

--- src/main/java/com/evanlennick/webserver/mimetypes/MimeType.java
+++ src/main/java/com/evanlennick/webserver/mimetypes/MimeType.java
@@ -9,6 +9,12 @@ public enum MimeType {
 
     IMG_JPG("image/jpeg", Arrays.asList("jpg", "jpeg", "jpe", "jfif", "jfif-tbnl")),
 
+    APPLICATION_JSON("application/json", Arrays.asList("json")),
+
+    APPLICATION_XML("application/xml", Arrays.asList("xml")),
+
+    APPLICATION_JAVASCRIPT("application/javascript", Arrays.asList("js")),
+
     TEXT_PLAIN("text/plain; charset=UTF-8", Arrays.asList("txt", "text", "c", "c++", "cc", "conf", "java", "log", "pl")),
 
     TEXT_HTML("text/html; charset=utf-8", Arrays.asList("html", "htm", "shtml", "htmls", "htx", "acgi"));
