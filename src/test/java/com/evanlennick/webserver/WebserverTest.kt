d8910ae3d74986b712faaaf5f1343ff8d541550c
Evan Lennick <elennick@gmail.com>
2017-07-09 21:34:34 -0400

    test cleanup

--- src/test/java/com/evanlennick/webserver/WebserverTest.kt
+++ src/test/java/com/evanlennick/webserver/WebserverTest.kt
@@ -1,16 +1,16 @@
 package com.evanlennick.webserver
 
 import org.apache.commons.httpclient.HttpClient
+import org.apache.commons.httpclient.HttpMethodBase
 import org.apache.commons.httpclient.methods.GetMethod
 import org.apache.commons.httpclient.methods.PostMethod
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.AfterClass
-import org.junit.Before
-import org.junit.BeforeClass
-import org.junit.Test
+import org.junit.*
 
 class WebserverTest {
     lateinit var client: HttpClient
+
+    lateinit var method: HttpMethodBase
 
     companion object {
         @JvmStatic
@@ -19,7 +19,7 @@ class WebserverTest {
         }
 
         @JvmStatic
-        @AfterClass fun teardown() {
+        @AfterClass fun stopServer() {
             Main.stop()
         }
     }
@@ -28,26 +28,27 @@ class WebserverTest {
         client = HttpClient()
     }
 
-    @Test fun test200Ok() {
-        val method = GetMethod("http://localhost:3353/index.html")
-        val statusCode = client.executeMethod(method)
+    @After fun teardown() {
         method.releaseConnection()
+    }
+
+    @Test fun test200Ok() {
+        method = GetMethod("http://localhost:3353/index.html")
+        val statusCode = client.executeMethod(method)
 
         assertThat(statusCode).isEqualTo(200)
     }
 
     @Test fun test404NotFound() {
-        val method = GetMethod("http://localhost:3353/doesntexist.html")
+        method = GetMethod("http://localhost:3353/doesntexist.html")
         val statusCode = client.executeMethod(method)
-        method.releaseConnection()
 
         assertThat(statusCode).isEqualTo(404)
     }
 
     @Test fun test501NotImplemented() {
-        val method = PostMethod("http://localhost:3353/whatever.html")
+        method = PostMethod("http://localhost:3353/whatever.html")
         val statusCode = client.executeMethod(method)
-        method.releaseConnection()
 
         assertThat(statusCode).isEqualTo(501)
     }
